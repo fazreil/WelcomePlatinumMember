a7a487453f81c054f0523fa838c6394fb69dfa4b
Fazreil Amreen <fazreil@gmail.com>
2021-01-23 17:33:24 +0000

    display query and footer for wolfram outputs

--- wolfram.py
+++ wolfram.py
@@ -1,9 +1,16 @@
 import requests
 import os
+import discord
 
 url = "https://api.wolframalpha.com/v1/result"
 
+def embed(string_to_embed):
+  embeded_message = discord.Embed()
+  embeded_message.set_footer(text=string_to_embed)
+  return embeded_message
+
 async def ask(msg, message):
+  await message.channel.send("```Query: "+msg+"```")
   querystring = {"i":msg, "appid":os.getenv('WOLFRAM')}
   response = requests.get(url, params=querystring)
-  await message.channel.send("```"+response.text+"```")
+  await message.channel.send(content="```"+response.text+"```", embed=embed("Source: wolframalpha.com"))
